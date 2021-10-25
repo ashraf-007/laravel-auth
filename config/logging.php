2de8d813cd41e682d09f24fd709acc7defccb295
ashraf <ashraf.benmou@gmail.com>
2021-10-25 22:23:23 +0100

    logs

--- config/logging.php
+++ config/logging.php
@@ -51,13 +51,13 @@ return [
         'stack' => [
             'driver' => 'stack',
             'channels' => ['single'],
-            'ignore_exceptions' => false,
+            // 'ignore_exceptions' => false,
         ],
 
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => env('LOG_LEVEL', 'debug'),
+            'driver' => 'errorlog',
+            // 'path' => storage_path('logs/laravel.log'),
+            'level' => env('debug'),
         ],
 
         'daily' => [
